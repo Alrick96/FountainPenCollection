818e7c122d2c2542235d8aba1cbeb89992d47235
Alrick96 <112648005+Alrick96@users.noreply.github.com>
2023-05-03 02:28:10 -0400

    update532023228

--- app/src/main/java/com/example/fountainpencollection/AccountSettings.kt
+++ app/src/main/java/com/example/fountainpencollection/AccountSettings.kt
@@ -1,11 +1,35 @@
 package com.example.fountainpencollection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+
 
 class AccountSettings : AppCompatActivity() {
+    lateinit var FB:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
+
+        var user = FirebaseAuth.getInstance().currentUser
+        var NewPassword = "fsfvsdfv"
+
+        user!!.updatePassword(NewPassword).addOnCompleteListener { task->
+            if (task.isSuccessful){
+
+            }
+            else{
+
+            }
+        }
+
     }
+
+
+
+
+
+
+
 }
