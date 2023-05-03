4717849e534e1447b9c5c3f5c7083c5bb4e28066
Alrick96 <112648005+Alrick96@users.noreply.github.com>
2023-05-03 04:37:34 -0400

    fd

--- app/src/main/java/com/example/fountainpencollection/AccountSettings.kt
+++ app/src/main/java/com/example/fountainpencollection/AccountSettings.kt
@@ -1,28 +1,41 @@
 package com.example.fountainpencollection
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 
 
 class AccountSettings : AppCompatActivity() {
-    lateinit var FB:FirebaseAuth
+    lateinit var NewPassword : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
 
         var user = FirebaseAuth.getInstance().currentUser
-        var NewPassword = "fsfvsdfv"
 
-        user!!.updatePassword(NewPassword).addOnCompleteListener { task->
-            if (task.isSuccessful){
+        var PasswordButton = findViewById(R.id.PasswordButton) as Button
 
+        PasswordButton.setOnClickListener{
+
+            NewPassword = findViewById(R.id.NewTextPassword)
+            var newPasswordSet = NewPassword.text.toString()
+            user!!.updatePassword(newPasswordSet).addOnCompleteListener { task->
+                if (task.isSuccessful){
+                    Toast.makeText(this@AccountSettings,"Password have been reset",Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+                else{
+                    Toast.makeText(this@AccountSettings,"it has failed",Toast.LENGTH_SHORT).show()
+                }
             }
-            else{
 
-            }
         }
+
+
 
     }
 
--- app/src/main/java/com/example/fountainpencollection/AppSettingsMenu.kt
+++ app/src/main/java/com/example/fountainpencollection/AppSettingsMenu.kt
@@ -7,5 +7,10 @@ class AppSettingsMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_settings_menu)
+
+
+        //change size of text
+        //change them
+
     }
 }
